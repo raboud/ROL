6078b1e8cb351ad4002d5baecac4127ec6bbba23
Robert Raboud <robert.raboud@randreng.com>
2019-12-07 14:18:20 -0500

    Removed Cloning and Equallity Checks

--- Src/Services/Catalog/Catalog.Domain/MetaDataEntity.cs
+++ Src/Services/Catalog/Catalog.Domain/MetaDataEntity.cs
@@ -4,21 +4,9 @@ using System.Collections.Generic;
 
 namespace ROL.Services.Catalog.Domain
 {
-	public class MetaDataEntity: IEquatable<MetaDataEntity>, ICloneable
+
+	public class MetaDataEntity
 	{
 		public Dictionary<string, object> MetaData { get; set; } = new Dictionary<string, object>();
-		public MetaDataEntity()
-		{
-		}
-
-		public bool Equals(MetaDataEntity other)
-		{
-			throw new NotImplementedException();
-		}
-
-		public object Clone()
-		{
-			return JsonConvert.DeserializeObject<MetaDataEntity>(this.ToString());
-		}
 	}
 }
